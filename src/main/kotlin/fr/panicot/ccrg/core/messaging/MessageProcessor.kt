b5eb77d775a67bf5e0ec95b3433342b4f7587af4
Aenyn <william@panicot.fr>
2017-02-15 22:42:18 +0100

    Fix url parsing

--- src/main/kotlin/fr/panicot/ccrg/core/messaging/MessageProcessor.kt
+++ src/main/kotlin/fr/panicot/ccrg/core/messaging/MessageProcessor.kt
@@ -9,11 +9,12 @@ class MessageProcessor(val message: Message) {
     fun processLinks(): MessageProcessor {
         val messageContent = message.content
         val messageSplit = messageContent.split(" ")
+        val urlRegex = Regex("(http(s)?:\\/\\/.)?(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#?&/=]*)")
         val messageProcessed = Message(message.id,message.timestamp,message.author,
                 messageSplit.map { part ->
-                    if(part.matches(Regex("(.+\\..+)"))){
+                    if(part.matches(urlRegex)){
                         val partWithHttp = if (part.startsWith("http")) part else "http://$part"
-                        "<a target=\"_blank\" href=$partWithHttp> $part </a>"
+                        "<a target=\"_blank\" href=$partWithHttp>$part</a>"
                     }
                     else part
                 }.joinToString(" "))

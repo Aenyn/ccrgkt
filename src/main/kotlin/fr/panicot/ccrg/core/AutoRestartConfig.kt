20d4f020f780ff5160fd4b0183f5da07a8aa742e
Aenyn <william@panicot.fr>
2017-03-26 16:24:13 +0200

    Auto restart at 4am

---
+++ src/main/kotlin/fr/panicot/ccrg/core/AutoRestartConfig.kt
@@ -0,0 +1,22 @@
+package fr.panicot.ccrg.core
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
+import org.springframework.scheduling.support.CronTrigger
+
+/**
+ * Created by William on 26/03/2017.
+ */
+@Configuration
+open class AutoRestartConfig {
+
+    val executor = ThreadPoolTaskScheduler()
+
+    @Bean
+    open fun startBots(): Boolean {
+        executor.initialize()
+        executor.schedule(Runnable{System.exit(0)}, CronTrigger("0 0 3 * * *"))
+        return true
+    }
+}

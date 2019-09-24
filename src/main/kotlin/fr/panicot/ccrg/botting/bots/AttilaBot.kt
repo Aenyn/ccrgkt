b2e76c7a65528d9c75592414a4f88441d2154563
Aenyn <william@panicot.fr>
2019-09-24 07:47:57 +0200

    Replace Attilabot spamming messages with hunbot repeatedly arrivings and fix time

--- src/main/kotlin/fr/panicot/ccrg/botting/bots/AttilaBot.kt
+++ src/main/kotlin/fr/panicot/ccrg/botting/bots/AttilaBot.kt
@@ -5,26 +5,24 @@ import fr.panicot.ccrg.core.messaging.MessageController
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
 import org.springframework.scheduling.support.CronTrigger
 import java.util.*
+import java.util.concurrent.atomic.AtomicInteger
 
 /**
  * Created by William on 16/03/2017.
  */
 class AttilaBot(messageController: MessageController, random: Random): Bot(messageController, random) {
-    val scheduler = ThreadPoolTaskScheduler()
-    val quoteList = Arrays.asList("YAAAAAAARGH !",
-            "LA OU JE PASSE L'HERBE NE REPOUSSE JAMAIS !",
-            "PLACE AU GRAND ATTILA !",
-            "JE SUIS LE FLEAU DE DIEU !",
-            "AAAAAAAARGH !",
-            "COUREZ POUR VOS VIES !",
-            "JE SUIS ATTILA LE HUN !")
-
-    fun randomPick() : String = quoteList[random.nextInt(quoteList.size)]
+    private val scheduler = ThreadPoolTaskScheduler()
+    private val armySize = AtomicInteger(0)
 
     override fun start() {
         scheduler.initialize()
         scheduler.schedule(Runnable{messageController.announceArrival("ATTILABOT", true)}, CronTrigger("0 11 9 * * *"))
-        scheduler.schedule(Runnable{messageController.sendMessage("ATTILABOT", randomPick())}, CronTrigger("1-59/4 11 9 * * *"))
+        scheduler.schedule(Runnable{
+            messageController.announceArrival("HUNBOT${armySize.getAndIncrement()}", true)
+        }, CronTrigger("1-59/8 11 9 * * *"))
         scheduler.schedule(Runnable{messageController.announceArrival("ATTILABOT", false)}, CronTrigger("0 12 9 * * *"))
+        scheduler.schedule(Runnable{for (i in 0..armySize.get()) {
+            messageController.announceArrival("HUNBOT$i", false)
+        }}, CronTrigger("0 12 9 * * *"))
     }
 }
